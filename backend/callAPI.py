f975068cf9b6ac7c26389f6d873fd613accec030
Hunter-Grimes <capegrimes1@gmail.com>
2024-03-06 12:40:43 -0500

    Separated testing from api call file

--- backend/callAPI.py
+++ backend/callAPI.py
@@ -3,7 +3,7 @@ import os
 import requests
 
 def getSumByName(name):
-    load_dotenv(find_dotenv())
+    load_dotenv()
     api_key = os.getenv('RIOT_API_KEY')
 
     requestURL = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"
@@ -18,9 +18,3 @@ def getSumByName(name):
         return response.json()
     else:
         raise Exception("Bad or No Response")
-
-
-if __name__ == "__main__":
-    print(getSumByName("lessjnglmorebush"))
-    api_key = os.getenv('RIOT_API_KEY')
-    print(requests.get('https://na1.api.riotgames.com/riot/account/v1/accounts/me' + '?api_key=' + api_key))
