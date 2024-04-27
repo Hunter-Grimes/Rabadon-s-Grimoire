d74350f4de79ec088a1fbed01601cc54f4d45f75
Seungwon (Michael) Jung <151700984+SeungwonMJ@users.noreply.github.com>
2024-04-27 19:57:41 -0400

    Update settings.py

--- RGlogin/RGlogin/settings.py
+++ RGlogin/RGlogin/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9cjxrl@uu)nxwe&rs6x7_3_zn+6g%)9o$-2bbv+fd+x*1oqgov'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
